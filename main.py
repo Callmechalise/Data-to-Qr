2d0358680e9d57d001b00102aff06966e9ffccb5
Pabitra.kr chalise <pabitrakumarchalise@gmail.com>
2026-07-02 23:06:38 +0545

    deploy commit

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import qrcode
 #basic file handelling
 file=open("data.txt","r")
 datas=(file.read())
-break_char=","
-list_of_data=datas.split(",")
+data_split_char=","#split logic add anything which suits your data
+list_of_data=datas.split(data_split_char)
 print(list_of_data)
 
 #Qr logic
 for data in list_of_data:
     qr=qrcode.make(data)
-    qr.save(f"{data.split()[0]}.png")
+    qr.save(f"{data.split()[0]}.png")#text till first space is filename
